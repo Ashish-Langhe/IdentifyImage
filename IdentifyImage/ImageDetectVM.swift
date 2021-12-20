db085fa6f1b4a12029cb461511e911a3ef4cb9b5
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2021-12-20 05:57:02 +0530

    Updated DetectViewManager class

--- IdentifyImage/ImageDetectVM.swift
+++ IdentifyImage/ImageDetectVM.swift
@@ -13,9 +13,20 @@ class ImageDetectVM: ObservableObject {
     
     var name: String = ""
     var manager: ImageDetectManager
-    @Published var getDetect: String
+    @Published var getDetect: String = ""
     
     init(manager:ImageDetectManager) {
         self.manager = manager
+    }
+    
+    func detect(_ name: String) {
+        let sourceImage = UIImage(named: name)
+        guard let resizedImage = sourceImage?.resizeImage(targerS: CGSize(width: 224, height: 224)) else {
+            fatalError("Not resized!!")
+        }
+        
+        if let label = self.manager.detectImage(resizedImage) {
+            self.getDetect = label
+        }
     }
 }
